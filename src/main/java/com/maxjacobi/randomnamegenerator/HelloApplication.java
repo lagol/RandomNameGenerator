5a936acb32174f6c12e9335d88440048b4870def
lagol <github@maxjacobi.com>
2022-09-13 18:53:45 +0200

    Commit

--- src/main/java/com/maxjacobi/randomnamegenerator/HelloApplication.java
+++ src/main/java/com/maxjacobi/randomnamegenerator/HelloApplication.java
@@ -1,17 +1,18 @@
 package com.maxjacobi.randomnamegenerator;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+
+        VBox root = new VBox();
+
+        Scene scene = new Scene(root);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
